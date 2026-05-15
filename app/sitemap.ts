4af789afb522c2f16f48cf5bbcbbdc11edbe123f
Car Access <CarAccess@MacBook-Air-de-Laurie.local>
2026-05-15 20:04:53 +0200

    SEO: sitemap + robots

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://arnaque-sms.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://arnaque-sms.vercel.app/arnaque-dhl",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://arnaque-sms.vercel.app/arnaque-chronopost",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://arnaque-sms.vercel.app/arnaque-colis",
+      lastModified: new Date(),
+    },
+  ];
+}
