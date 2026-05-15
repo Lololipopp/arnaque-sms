52c29d7e46200180e76e5791213d1ad1aba8c934
Car Access <CarAccess@MacBook-Air-de-Laurie.local>
2026-05-15 22:17:17 +0200

    Update pages

--- app/arnaque-chronopost/page.tsx
+++ app/arnaque-chronopost/page.tsx
@@ -9,21 +9,20 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <PageWrapper title="Faux SMS Chronopost : comment les reconnaître">
+    <PageWrapper title="Faux SMS Chronopost : comment les reconnaître ?">
 
       <main>
-        <h1>Arnaque Chronopost SMS : comment reconnaître un faux message</h1>
 
         <p>
           Les faux SMS Chronopost sont très fréquents en France et visent à voler vos données personnelles ou bancaires.
         </p>
 
-        <h2>Exemple d’arnaque Chronopost</h2>
+        <h2>Exemple d’arnaque Chronopost :</h2>
         <p>
           “Votre colis est en attente. Paiement de 2,99€ requis pour livraison.”
         </p>
 
-        <h2>Signes d’alerte</h2>
+        <h2>Signes d’alerte :</h2>
         <ul>
           <li>Lien suspect ou raccourci</li>
           <li>Urgence artificielle</li>
@@ -35,7 +34,6 @@ export default function Page() {
           Chronopost ne demande jamais de paiement par SMS.
         </p>
 
-        <h2>Vérifier un SMS suspect</h2>
         <Link href="/">
           🔎 Tester un SMS suspect
         </Link>
